cc49876985b2ecaef393e18e8cc46c10dc00a955
mouse1031 <117873987+mouse1031@users.noreply.github.com>
2022-11-10 16:38:47 +0800

    Update gitTest.java

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello,git!4");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
